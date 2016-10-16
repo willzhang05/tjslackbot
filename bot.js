8bc10a7b5ed64de620586741943fa86a9c242cb8
William Zhang <willzhang05@gmail.com>
2016-10-16 00:05:41 -0400

    Fixed and added a lot of new commands

--- bot.js
+++ bot.js
@@ -1,142 +1,134 @@
-var cli = require("node-cmd");
-var await = require("await");
+var exec = require("child_process").exec;
 var safeEval = require("safe-eval");
-var output;
-//var output = runCommand(cmd, cmdArgs, function(result) { return result; });
-
-/*function runCommandCallback(result) {
-    console.log(cmd + " arg:" + cmdArgs);
-    console.log(output);
-    return result;
-}*/
-
-function runCommand(cmd, args, callback) {
-    var spawn = require("child_process").spawn;
-    var command = spawn(cmd, args);
-    console.log("1 " + cmd + " " + args);
-    command.stdout.on("data", function(data) {
-        output = data.toString();
-    });
-    command.on("error", function(err){throw err});
-    command.on("close", function(code) {
-        console.log("2 " + output);
-        return callback(output);
-    });
-}
-/*runCommand("nslookup", ["google.com"]);
-function runCommand(cmd, args) {
-    cli.get(cmd, function(data) {
-            output = data;
-        }
-    );
-}*/
 
 module.exports = function (req, res, next) {
     var userName = req.body.user_name;
     var args = req.body.text.split(" ");
     var botPayload;
-    console.log(args);
     var cmdName = args[0].substring(1);
+    cmdHandler(userName, cmdName, args, function(data) {
+        botPayload = data;
+        console.log(botPayload);
+        if(botPayload["text"] === "") {
+            botPayload["text"] = "Oops! An error occured.";
+        }
+        if (userName !== 'slackbot') {
+            return res.status(200).json(botPayload);
+        } else {
+            return res.status(200).end();
+        }
+     });
+}
+
+function cmdHandler(userName, cmdName, args, callback) {
+    var errStr = "Invalid syntax";
     try {
         if(cmdName === "help") {
-            botPayload = help(args);
+            callback(help(args));
         }
         else if(cmdName === "hello") {
-            botPayload = { text : "Hello, " + userName + "!" };
-        }
-        else if(cmdName === "nslookup") {
-            botPayload = { text : nslookup(args)};//runCommand(cmdName, [args[1]], function(result) { return result; });//nslookup(args);
-            console.log("3 " + nslookup(args));
-        }
-        else if(cmdName === "calc") {
-            botPayload = { text : safeEval(args[1].toString().replace("/([^\d\w+\-*\/\\\(\)\.])/", "")).toString() };
-            console.log(botPayload["text"]);
-        }
-        else if(cmdName === "lmgtfy") {
-            var search = "";
-            for(var i = 1; i < args.length; i++) {
-                search += args[i] + "+";
-            }
-            botPayload = { text : "http://lmgtfy.com/?q=" + search.substring(0, search.length - 1) };
+            callback({ text : "Hello, " + userName + "!" });
         }
         else if(cmdName === "g") {
-            var search = "";
-            for(var i = 1; i < args.length; i++) {
-                search += args[i] + "+";
+            if(args.length > 1) {
+                var search = "";
+                for(var i = 1; i < args.length; i++) {
+                    search += args[i] + "+";
+                }
+                callback({ text : "http://google.com/search?q=" + search.substring(0, search.length - 1) });
+            } else {
+                throw errStr;
             }
-            botPayload = { text : "http://google.com/search?q=" + search.substring(0, search.length - 1) };
         }
         else if(cmdName === "date") {
             var offset = -4;
-            botPayload = { text : new Date( new Date().getTime() + offset * 3600 * 1000).toUTCString().replace( / GMT$/, "" ) };
+            callback({ text : new Date( new Date().getTime() + offset * 3600 * 1000).toUTCString().replace( / GMT$/, "" ) });
+        }
+        else if(cmdName === "ping") {
+            ping(args, function(data) { callback(data); });  
+        }
+        else if(cmdName === "uptime") {
+            uptime(args, function(data) { callback(data); });  
+        }
+        else if(cmdName === "calc") {
+            if(args.length > 1) {
+                var exp = "";
+                for(var i = 1; i < args.length; i++) {
+                    exp += args[i].toString().replace("/([^\d\w+\-*\/\\\(\)\.])/", "");
+                }
+                callback({ text : safeEval(exp) });
+            } else {
+                throw errStr;
+            }
+        } else if(cmdName === "choose") {
+            if(args.length == 4) {
+                var prob = Math.random();
+                if(Math.random() < 0.5) {
+                    callback({ text : args[1] });
+                } else {
+                    callback({ text : args[3] });
+                }
+            } else {
+                throw errStr;
+            }
+        } else if(cmdName === "pester") {
+            if(args.length > 2) {
+                var user = "@" + args[1];
+                var msg = "";
+                for(var i = 2; i < args.length; i++) {
+                    msg += " " + args[i];
+                }
+                msg = user + msg + msg + msg;
+                callback({ text : msg });
+            } else {
+                throw errStr;
+            }
         }
         else {
-            botPayload = { text : "Invalid command. Run !help to see supported commands." }
+            callback({ text : "Invalid command. Run !help to see supported commands." });
         }
     } catch (e) {
-        botPayload = { text : e.toString() };
-    }
-    // avoid infinite loop
-    if (userName !== 'slackbot') {
-        return res.status(200).json(botPayload);
-    } else {
-        return res.status(200).end();
+        callback({ text : e.toString() });
     }
 }
+
+/*function execute(command, callback) {
+    exec(command, function(error, stdout, stderr){ callback(stdout); });
+}*/
 
 function help(args) {
     return {
-        "text" : "Commands: !help, !hello, !nslookup <domain>, !calc <expression>, !lmgtfy <thing>, !g <thing>, !date"
+        "text" : "Commands: !help, !hello, !calc <expression>, !g <thing>, !date, !ping <domain>, !uptime, !choose <thing 1> or <thing 2>, !pester <username> <thing>"
     };
 }
 
-function nslookup(args) {
-    res = { text : "" };
+function ping(args, callback) {
     if(args.length == 1 || args[1].match("/([^.\d\w])/g")) {
-        res["text"] = "Invalid syntax";
-        return res;
-    }
-    var domain = args[1];
-    cmd = "nslookup"
-    if(args[1].includes("|")) {
-        domain = args[1].split("|")[1];
-        domain = domain.slice(0, domain.length - 1);
-    }
-    res = { text : await("data") };
-    runCommand(cmd, [domain], function(result) { 
-        res = result;
-        //res["text"].keep("data", result);
-        return result;
-    });
-    //res["text"] = output;
-    return res;
-}
-
-
-function start(token) { 
-    request
-        .get("https://slack.com/api/rtm.start", {
-            'auth': {
-                'token':getConfig("token"),
+        callback({ text : "Invalid syntax" });
+    } else {
+        var domain = args[1];
+        cmd = "ping"
+        if(domain.includes("|")) {
+            domain = args[1].split("|")[1];
+            domain = domain.slice(0, domain.length - 1);
+        }
+        exec(cmd + " -c 1 " + domain, function(error, stdout, stderr) { 
+            if(error) {
+                callback({text : stderr});
+            } else {
+                callback({text : stdout});
             }
-        })
-        .on('response', function(response) {
-            console.log(response.statusCode);
-            console.log(response.headers['content-type']);
-        })
-        .pipe(fs.createWriteStream('state.json'));
+        });
+    }
 }
 
-var config;
-function getConfig(arg) {
-    fs.readFile('./config.json', function read(err, data) {
-        if(err) {
-            throw err;
+function uptime(args, callback) {
+    cmd = "uptime"
+    exec(cmd, function(error, stdout, stderr) { 
+        if(error) {
+            callback({text : stderr});
+        } else {
+            callback({text : stdout});
         }
-        if(typeof config != 'object') {
-            config = JSON.parse(data);
-        }
-        console.log(config[arg]);
-        return config[arg];
     });
 }
